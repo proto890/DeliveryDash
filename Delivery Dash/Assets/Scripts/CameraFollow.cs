b2a0858b721e9a8b1b302833ab7b0158fb18db9b
Joshua Firkin <joshuafirkin@hotmail.co.uk>
2017-01-27 20:59:14 +0000

    Added Basic Movement
    I've added a basic movement and rotation script, as well as a camera
    follow script.
    All graphics at the moment are completely placeholder.

---
+++ Delivery Dash/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    private Transform playerTransform;
+    private float followSpeed = 0.01f;
+
+    private Vector3 velocity = Vector3.zero;
+
+	void Start ()
+    {
+        playerTransform = GameObject.Find("Player").transform;
+	}
+
+
+    void LateUpdate()
+    {
+        Vector3 targetPos = playerTransform.position;
+        targetPos.y = transform.position.y;
+        targetPos.z = playerTransform.position.z - 10;
+
+        transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, followSpeed);
+    }
+}
---
+++ Delivery Dash/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody rb;
+    private float moveSpeed = 20;
+    private float rotationSpeed = 20;
+
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	void Update ()
+    {
+        Drive();
+	}
+
+    void Drive()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime, 0, Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime);
+        Vector3.Normalize(movement);
+        transform.position += movement;
+
+        if (movement != Vector3.zero)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), rotationSpeed * Time.deltaTime);
+        }
+    }
+}
