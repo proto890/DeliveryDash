4e1e884418ff7a42eb32152e5c1c14a59a788cc0
Joshua Firkin <joshuafirkin@hotmail.co.uk>
2017-01-28 16:01:21 +0000

    Made improvements to the current movement.
    I made improvements to the way the camera moves to follow the player
    (much smoother now).
    I made the player movement a lot smoother and added a ground check using
    raycasting which disables the trail renderers  when the player is not on
    the ground.
    Also made a little ramp for testing purposes.

--- Delivery Dash/Assets/Scripts/CameraFollow.cs
+++ Delivery Dash/Assets/Scripts/CameraFollow.cs
@@ -3,23 +3,29 @@ using System.Collections;
 
 public class CameraFollow : MonoBehaviour {
 
+    //Gets players transform component.
     private Transform playerTransform;
-    private float followSpeed = 0.01f;
+    //Defines the speed of the camera.
+    private float followSpeed = 10f;
 
-    private Vector3 velocity = Vector3.zero;
 
 	void Start ()
     {
+        //Defines where to find the players transform component.
         playerTransform = GameObject.Find("Player").transform;
 	}
 
-
+    //Executes after both update and fixed update.
     void LateUpdate()
     {
+        //Sets target position X as the player position X.
         Vector3 targetPos = playerTransform.position;
+        //Sets target position Y to be the cameras Y value.
         targetPos.y = transform.position.y;
+        //Sets target position Z as the player position Z.
         targetPos.z = playerTransform.position.z - 10;
 
-        transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, followSpeed);
+        //Lerps the cameras transform.position towards the players X and Z coordinates.
+        transform.position = new Vector3(Mathf.Lerp(transform.position.x, targetPos.x, followSpeed * Time.deltaTime), targetPos.y, Mathf.Lerp(transform.position.z, targetPos.z, followSpeed * Time.deltaTime));
     }
 }
--- Delivery Dash/Assets/Scripts/PlayerController.cs
+++ Delivery Dash/Assets/Scripts/PlayerController.cs
@@ -2,30 +2,97 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
+    //Which layer the Raycast will hit (Currently set to ground layer).
+    public LayerMask groundCheckLayer = 8;
 
+    //Declares rigidbody.
     private Rigidbody rb;
+    //Delares an array of trail renderers (2).
+    private TrailRenderer[] trailRend = new TrailRenderer[2];
+    //Declares the players move speed.
     private float moveSpeed = 20;
+    //Declares the speed which the player will rotate at when mpving in a different direction.
     private float rotationSpeed = 20;
 
 	void Start ()
     {
+        //Defines rigidbody.
         rb = GetComponent<Rigidbody>();
+        //Defines both trail renderers.
+        trailRend[0] = transform.Find("Trail[0]").GetComponent<TrailRenderer>();
+        trailRend[1] = transform.Find("Trail[1]").GetComponent<TrailRenderer>();
+        //Trail renderer null check.
+        if (trailRend[0] == null || trailRend[1] == null)
+        {
+            Debug.Log("Trail Renderer not found");
+        }
 	}
 	
 	void Update ()
     {
         Drive();
+        GroundCheck();
 	}
 
+    //Moves player.
     void Drive()
     {
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime, 0, Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime);
-        Vector3.Normalize(movement);
-        transform.position += movement;
+        //Defines a movement vector.
+        Vector3 groundMovement = new Vector3(Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime, 0, Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime);
+        //Normalizes vector so that the player can not move faster diagonally.
+        Vector3.Normalize(groundMovement);
 
-        if (movement != Vector3.zero)
+        //moves position.
+        transform.position += groundMovement;
+
+        //Checks if there is no movement input
+        if (groundMovement != Vector3.zero)
         {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), rotationSpeed * Time.deltaTime);
+            //rotates towards movement direction.
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(groundMovement), rotationSpeed * Time.deltaTime);
+        }
+    }
+
+    //Checks if player is on ground.
+    bool GroundCheck()
+    {
+        //Declares a hit.
+        RaycastHit hit;
+
+        //Draws the raycast so that I'm not blind while guessing the length.
+        Debug.DrawRay(transform.position, -Vector3.up * 0.5f, Color.green, 0.2f);
+
+        //Checks if raycast has hit anything.
+        if (Physics.Raycast(transform.position, -Vector3.up, out hit, 0.5f))
+        {
+            Debug.Log("Floor was hit.");
+
+            //if either of the trail renderers are not enabled.
+            if (trailRend[0].enabled == false || trailRend[1].enabled == false)
+            {
+                //Enable both trail renderers in a for loop.
+                for (int i = 0; i < trailRend.Length; i++)
+                {
+                    //Enable both.
+                    trailRend[i].enabled = true;
+                }
+            }
+
+            //returns true informing that the ray did infact hit.
+            return true;
+        }
+        //If the raycast did not hit anything.
+        else
+        {
+            //Disable both of the trail renderers in a for loop.
+            for (int i = 0; i < trailRend.Length; i++)
+            {
+                //Disable current renderer.
+                trailRend[i].enabled = false;
+            }
+
+            //returns false informing that the ray did not hit.
+            return false;
         }
     }
 }
